2dcdcfba4ce55d5d014956abb396e4e03d295c56
EMIL WIPPLINGER <emil.wipplinger@gmail.com>
2023-05-31 09:28:15 +0200

    Cleanup and Comments

--- src/FindPeaks.py
+++ src/FindPeaks.py
@@ -8,21 +8,20 @@ import math
 
 class FindPeaks:
     def __init__(self):   
-        self.scan_col = 0
-        self.time_col = 0
-        self.baselines = pd.DataFrame
-        self.df_peaks = pd.DataFrame
-        self.df_raw = pd.DataFrame
+        self.scan_col = 0           #column index for peak scanning
+        self.time_col = 0           #column index for time 
+        self.df_peaks = pd.DataFrame    #dataframe for peak data
+        self.df_raw = pd.DataFrame      #dataframe for raw data
         pass
     
 
     def importData(self, a_file_name):
         """Loads a .csv file into the class own dataframe"""
-        if (open(a_file_name, "r") == None):
+        if (open(a_file_name, "r") == None):        #check file
             print("FileName not valid!")
             return
         
-        self.df_raw = pd.read_csv(a_file_name, sep = ";", header = None, names = None)
+        self.df_raw = pd.read_csv(a_file_name, sep = ";", header = None, names = None)  #read csv to dataframe
         if(self.df_raw.empty):
             print("DataFile was empty!")
             return
@@ -33,13 +32,13 @@ class FindPeaks:
 
     def detectPeaks(self, a_column=0):
         """Scans the selected column of source-dataframe for Peaks and stores the peak-information in a seperate dataframe"""
-        self.scan_col = a_column
-        self.time_col = 1
+        self.scan_col = a_column    #define column for scanning
+        self.time_col = 1           #TODO time column ist still hard coded!
         #%% Detect Peaks
-        _indices = []    
-        _up = -1
-        _curr = 0 
-        _prev = self.df_raw.iloc[0][self.scan_col]
+        _indices = []                 #indices of the peaks
+        _up = -1                      #location of an upwards flank
+        _curr = 0                     #current data value in the loop
+        _prev = self.df_raw.iloc[0][self.scan_col]          #previous data value in the loop
 
         for i in range(1, len(self.df_raw[self.scan_col])):
             _curr = self.df_raw.iloc[i][self.scan_col]          #get 2 adjacent values from data
@@ -85,7 +84,7 @@ class FindPeaks:
         pass
 
 
-    def get_absolute(self):
+    def get_absolute(self):     #this method has no use for now...
         max = self.df_raw[0]["value"]
         if self.df_peaks.empty():          
             for index, row in self.df_raw.iterrows():
@@ -103,17 +102,17 @@ class FindPeaks:
         pass
 
     def add_baseline(self):
-        self.baselines = pd.DataFrame(columns=[])
-        _peak_indices = self.df_peaks.index.values.tolist()
-        _rbase_list = []
-        _lbase_list = []
-        _delta_base_list = []
+        """Calculates the vertical distance from each peak to its own baseline"""
+        _peak_indices = self.df_peaks.index.values.tolist()     #get all indices of df_peaks
+        _rbase_list = []        #the bases right to each peak
+        _lbase_list = []        #the bases left to each peak
+        _delta_base_list = []   #the distances of each peak to the baseline
 
         for i in range(0, len(_peak_indices)):
             _step = _peak_indices[i]
             _curr = 0
             _next = 0
-            while _step < 2 + len(self.df_raw[self.scan_col]):
+            while _step < 2 + len(self.df_raw[self.scan_col]):      #scan for upwards flank on the right side of the peak
                 _curr = self.df_raw.iloc[_step][self.scan_col]
                 _next = self.df_raw.iloc[_step + 1][self.scan_col]  
                 if _next > _curr:
@@ -122,7 +121,7 @@ class FindPeaks:
                 _step += 1
 
             _step = _peak_indices[i]
-            while _step > 0:
+            while _step > 0:            #scan for upwards flank on the left side of the peak
                 _curr = self.df_raw.iloc[_step][self.scan_col]
                 _next = self.df_raw.iloc[_step - 1][self.scan_col]  
                 if _next > _curr:
@@ -130,12 +129,12 @@ class FindPeaks:
                     break
                 _step -= 1
             
-            _x = _peak_indices[i]
-            _y = self.df_raw.iloc[_peak_indices[i]][self.scan_col]
-            _xl = _lbase_list[i]
-            _yl = self.df_raw.iloc[_lbase_list[i]][self.scan_col]
-            _xr = _rbase_list[i]
-            _yr = self.df_raw.iloc[_rbase_list[i]][self.scan_col]
+            _x = _peak_indices[i]       #x pos of the peak 
+            _y = self.df_raw.iloc[_peak_indices[i]][self.scan_col]  #y pos of the peak
+            _xl = _lbase_list[i]        #x pos of the left base                            
+            _yl = self.df_raw.iloc[_lbase_list[i]][self.scan_col]   #y pos of the left base
+            _xr = _rbase_list[i]        #x pos of the right base
+            _yr = self.df_raw.iloc[_rbase_list[i]][self.scan_col]   #y pos of the right base
 
             _dbx = _xl - _xr
             _dby = _yl - _yr
@@ -168,42 +167,34 @@ class FindPeaks:
         "Visualizes the data with marked peak locations"
         _peak_indices = self.df_peaks.index.values.tolist()
 
-        for idx, row in self.df_peaks.iterrows():
-            _x = idx
-            _y = self.df_raw.iloc[_x][self.scan_col]
-
-            _xl = row["Left Base"]
-            _yl = self.df_raw.loc[_xl][self.scan_col]
-
-            _xr = row["Right Base"]
-            _yr = self.df_raw.loc[_xr][self.scan_col]
-
-            _delta_base = row["Vert. distance to Baseline"]
-
-            plt.plot([_xl, _xr], [_yl, _yr], color='m', linestyle='--', linewidth=1)
-            plt.plot([_x, _x], [_y, _y - _delta_base], color='m', linestyle='--', linewidth=1)
-
-
-
+        if ~ "Left Base" in self.df_peaks.columns:
+            return
+            
         self.df_raw[self.scan_col].plot(figsize=(20,8))
         self.df_raw.iloc[_peak_indices][self.scan_col].plot(style='.', lw=10, color='red', marker="v")
         ##self.df_raw.iloc[_rbase_list + _lbase_list][self.scan_col].plot(style='.', lw=10, color='green', marker="^")
-        pass
+        
+        if ~ "Left Base" in self.df_peaks.columns:
+            return
+
+        for idx, row in self.df_peaks.iterrows():
+                _x = idx
+                _y = self.df_raw.iloc[_x][self.scan_col]
+
+                _xl = row["Left Base"]
+                _yl = self.df_raw.loc[_xl][self.scan_col]
+
+                _xr = row["Right Base"]
+                _yr = self.df_raw.loc[_xr][self.scan_col]
+
+                _delta_base = row["Vert. distance to Baseline"]
+
+                plt.plot([_xl, _xr], [_yl, _yr], color='m', linestyle='--', linewidth=1)
+                plt.plot([_x, _x], [_y, _y - _delta_base], color='m', linestyle='--', linewidth=1)
                  
 
 
-script_dir = os.path.dirname(__file__) #absolute dir the script is in
-rel_path = "../data/" #realtive path of the .csv file
-file1 = "test.csv"
-abs_file_path = os.path.join(script_dir, rel_path, file1)
 
-myPeaks = FindPeaks()
-myPeaks.importData(abs_file_path)
-myPeaks.detectPeaks(0)
-myPeaks.add_baseline()
-myPeaks.plotPeaks()
-
-plt.show(block = True)
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
-import matplotlib as mp
-import pandas as pd
+import FindPeaks
 import os
+from matplotlib import pyplot as plt
 
 script_dir = os.path.dirname(__file__) #absolute dir the script is in
 rel_path = "../data/" #realtive path of the .csv file
-file1 = "01_Ruhe.csv"
-
+file1 = "test.csv"
 abs_file_path = os.path.join(script_dir, rel_path, file1)
-headers = ["Zeit", "Messwerte [mV]"]
 
+myPeaks = FindPeaks.FindPeaks()
+myPeaks.importData(abs_file_path)
+myPeaks.detectPeaks(0)
+myPeaks.add_baseline()
+myPeaks.plotPeaks()
+
+plt.show(block = True)
